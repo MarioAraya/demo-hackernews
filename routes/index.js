4f0f4f439c69c621ec564422b6382f5178b10b1a
Mario Araya <mario.araya@globant.com>
2018-07-26 13:51:46 -0400

    onpen story_links in blank page

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,18 @@ const mockNews = require('./mock.json')
 const mock2 = [{
   created_at: "2018-07-19T21:45:05.000Z",
   author: "nodesocket",
-  story_title: "Google Cloud Downtime Postmortem"
+  story_title: "Google Cloud Downtime Postmortem",
+  story_url: "https://status.cloud.google.com/incident/cloud-networking/18012?m=1"
 },{
   created_at: "2018-07-17T21:45:05.000Z",
   author: "nobody",
-  story_title: "Etc"
+  story_title: "Etc",
+  story_url: "https://www.economist.com/science-and-technology/2018/07/21/python-has-brought-computer-programming-to-a-vast-new-audience"
 },{
   created_at: "2018-07-12T21:45:05.000Z",
   author: "author3",
-  story_title: "Title asdf"
+  story_title: "Title asdf",
+  story_url: "https://tyk.io/blog/rest-never-crud/"
 }]
 
 /* GET home page. */
