eda13985863e9c848b6af09415ee215d50fa2ce6
Mario Araya <mario.araya@globant.com>
2018-07-28 03:24:04 -0400

    /dataonly endpoint to get JSON + / working and rendering

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mongoMethods = require('../src/mongo-methods')
 
 // TODO: delete mock code, Get from API
 const mockNews = require('./mock.json')
@@ -22,8 +23,18 @@ const mock2 = [{
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HN News', subtitle: 'We <3 hacker news!', feed: mock2 });
-});
+  mongoMethods.getAllNewsFromDb(function(result) {
+    console.log('getAllNewsFromDb OK !!!');
+    res.render('index', { title: 'HN News', subtitle: 'We <3 hacker news!', feed: result });
+  }
+)});
+
+/* GET just data */
+router.get('/dataonly', function(req, res, next) {
+  mongoMethods.getAllNewsFromDb(function(result) {
+    res.send(result)    
+  }
+)});
 
 /* GET posts mock */
 router.get('/json-mock', function(req, res, next) {
