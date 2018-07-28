f23acbc309df7e178a64faf51ac4e6e6728ab1d2
Mario Araya <mario.araya@globant.com>
2018-07-28 14:20:32 -0400

    mock data and endpoint removed

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,6 @@ var express = require('express');
 var router = express.Router();
 var mongoMethods = require('../src/mongo-methods')
 
-// TODO: delete mock code, Get from API
-const mockNews = require('./mock.json')
-const mock2 = [{
-  created_at: "2018-07-19T21:45:05.000Z",
-  author: "nodesocket",
-  story_title: "Google Cloud Downtime Postmortem",
-  story_url: "https://status.cloud.google.com/incident/cloud-networking/18012?m=1"
-},{
-  created_at: "2018-07-17T21:45:05.000Z",
-  author: "nobody",
-  story_title: "Etc",
-  story_url: "https://www.economist.com/science-and-technology/2018/07/21/python-has-brought-computer-programming-to-a-vast-new-audience"
-},{
-  created_at: "2018-07-12T21:45:05.000Z",
-  author: "author3",
-  story_title: "Title asdf",
-  story_url: "https://tyk.io/blog/rest-never-crud/"
-}]
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   mongoMethods.getAllNewsFromDb(function(result) {
@@ -35,10 +16,5 @@ router.get('/dataonly', function(req, res, next) {
     res.send(result)    
   }
 )});
-
-/* GET posts mock */
-router.get('/json-mock', function(req, res, next) {
-  res.json(mockNews)
-});
 
 module.exports = router;
